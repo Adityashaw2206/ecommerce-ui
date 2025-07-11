a3cc780d4b25010a9d1210a4b353a56a518b4baf
Adityashaw2206 <shawaditya137@gmail.com>
2025-07-11 22:01:41 +0530

    commit

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { ShopContext } from "../../context/ShopContext";
-import RelatedProducts from "../../components/RelatedProducts";
-import { Link } from "react-router-dom";
+import { ShopContext } from "../../Context/ShopContext.jsx";
+import RelatedProducts from "../../components/RelatedProducts.jsx";
+// import { Link } from "react-router-dom";
 const Product = () => {
   const { productId } = useParams();
   const { products, addToCart, navigate } = useContext(ShopContext);
